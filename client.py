ae0aad3efd6588536780c93cb9a441a2400c9665
ykarocin <ysa@cin.ufpe.br>
2025-03-31 16:25:37 -0300

    Tentativa inicial

--- client.py
+++ client.py
@@ -30,20 +30,39 @@ def convert_action_number(action : int) -> str:
         return "jump"
     
 def better_action(state : int) -> str:
+    
+    return np.argmax(q_table[state])
+    # if q_table[state, 0] > q_table[state, 1] and q_table[state, 0] > q_table[state, 2]:
+    #     return 0
+    # elif q_table[state, 1] > q_table[state, 0] and q_table[state, 1] > q_table[state, 2]:
+    #     return 1
+    # else:
+    #     return 2
+ 
+# Atualizando a q_table    
+def update(oldState, action, newState, reward):
+    
+    if (action == "left"): 
 
-    if q_table[state, 0] > q_table[state, 1] and q_table[state, 0] > q_table[state, 2]:
-        return 0
-    elif q_table[state, 1] > q_table[state, 0] and q_table[state, 1] > q_table[state, 2]:
-        return 1
-    else:
-        return 2
+        action = 0
+    
+    elif (action == "right"): 
 
+        action = 1
+    
+    elif (action == "jump"): 
+
+        action = 2
+    
+    sPrime = int(newState,2)
+    bellman = bellman_equation(reward, sPrime, gamma)
+    q_table[oldState, action] += alfa * (bellman - q_table[oldState, action])
 curr_state = 0
 curr_reward = -14
 
-alfa = 0.1 # Taxa aprendizado (no nosso caso, o agente aprende de forma lenta, porém estável). 
-gamma = 0.9 # Peso das recompensas futuras (no nosso caso, o agente planeja a longo prazo e considera recompensas futuras mais importantes)
-episilon = 0 # Isso faz parte do epsilon greedy strategy. 
+alfa = 0.2 # Taxa aprendizado (no nosso caso, o agente aprende de forma lenta, porém estável). 
+gamma = 0.7 # Peso das recompensas futuras (no nosso caso, o agente planeja a longo prazo e considera recompensas futuras mais importantes)
+episilon = 0.3 # Isso faz parte do epsilon greedy strategy.     
 
 while (True): 
 
@@ -63,10 +82,14 @@ while (True):
 
         action = convert_action_number(better_action(curr_state))
 
+    episilon = max(episilon * 0.995, 0.01)
     print(action)
 
     state, reward = cn.get_state_reward(s, action)
 
     print(reward)
+    
+    update(curr_state, action, state, reward)
+    curr_state = int(state, 2)
 
     np.savetxt('resultado.txt', q_table, fmt="%f")  
