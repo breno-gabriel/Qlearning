b9d29e119fad92b00da4aed6cd9e44e75ed2903d
undecillion <86275510+undecillion@users.noreply.github.com>
2025-04-06 22:01:40 -0300

    Refactor client.py for improved readability and update resultado.txt with new Q-values

--- client.py
+++ client.py
@@ -1,91 +1,116 @@
-import connection as cn 
-import random 
+import connection as cn
+import random
 import numpy as np
 
-# Conectando com a porta. 
+#? Conectando com a porta.
 s = cn.connect(2037)
 
-# usamos a biblioteca numpy para ler o 'resultado.txt' como uma matriz
-q_table = np.loadtxt('resultado.txt')
+#? A Q-Table, contendo os valoes da função Q usamos a biblioteca numpy para ler o 'resultado.txt' como uma matriz
+q_table = np.loadtxt("resultado.txt")
 
-# Funções uteis. 
-def bellman_equation( r, s_prime, gamma):
-    
+#¢ 1. conversões --
+
+def convert_action_number(action: int) -> str:
+
+    if action == 0:
+
+        return "left"
+
+    elif action == 1:
+
+        return "right"
+
+    elif action == 2:
+
+        return "jump"
+
+def convert_number_action(action: str) -> int:
+
+    if action == "left":
+
+        return 0
+
+    elif action == "right":
+
+        return 1
+
+    elif action == "jump":
+
+        return 2
+
+#¢ 1. --
+
+#¢ 2. mecanismos de aprendizado --
+#? Seleciona melhor ação para um determinado estado, a que o agente irá tomar
+
+def better_action(state: int) -> int:
+
+    return np.argmax(q_table[state])
+
+#? A função Bellman é a função de valor de um estado, que é a soma da recompensa imediata e o valor máximo do próximo estado (iterativamente aprimorada) com um desconto gamma
+
+def bellman_equation(r, s_prime, gamma):
+
     max_q_prime = np.max(q_table[s_prime])
     points = r + gamma * max_q_prime
     return points
 
-def convert_action_number(action : int) -> str:
+# Atualizando a q_table
 
-    if (action == 0): 
-
-        return "left"
-    
-    elif (action == 1): 
-
-        return "right"
-    
-    elif (action == 2): 
-
-        return "jump"
-    
-def better_action(state : int) -> str:
-    
-    return np.argmax(q_table[state])
- 
-# Atualizando a q_table    
 def update(oldState, action, newState, reward):
-    
-    if (action == "left"): 
 
-        action = 0
-    
-    elif (action == "right"): 
+    action = convert_number_action(action)
 
-        action = 1
-    
-    elif (action == "jump"): 
-
-        action = 2
-    
     value = bellman_equation(reward, newState, gamma)
-    q_table[oldState, action] += alfa * (value - q_table[oldState, action])
-    
+    q_table[oldState, action] += alpha * (value - q_table[oldState, action])
+
+#¢ 2. --
+
+#¢ 3. variáveis --
+
 curr_state = 0
 
-alfa = 0.01 # Taxa de aprendizado, atualmente em 0.01 para evitar que sobrescrições bruscas sejam feitas na q_table durante a demonstração 
-gamma = 0.95 # Peso das recompensas futuras, fator de desconto na equação
-episilon = 0.01 # Isso faz parte do epsilon greedy strategy. Atualmente em 0.01 para evitar ações aleatórias     
+#! HIPERPARÂMETROS --
+alpha = 0.01  #! Taxa de aprendizado, atualmente em 0.01 para evitar que sobrescrições bruscas sejam feitas na q_table durante a demonstração
+gamma = 0.95  #! Peso das recompensas futuras, fator de desconto na equação
+epsilon = 0.01  #! Isso faz parte do epsilon greedy strategy. Atualmente em 0.01 para evitar ações aleatórias
+#! --
 
-while (True): 
+#¢ 3. --
 
-    print('=========================== Runnig the project ==============================================================')
+# ANCHOR #!main --
 
-    # epsilon greedy strategy (o agente decidirá se irá explorar o ambiente ou buscar por uma estratégia já conhecida)
+while True:
+
+    print(
+        "=========================== Running the project =============================================================="
+    )
+
+    #! epsilon greedy strategy (o agente decidirá se irá explorar o ambiente ou buscar por uma estratégia já conhecida)
     random_number = random.random()
-    if random_number < episilon: 
+    if random_number < epsilon:
 
-        # Aqui o agente busca explorar o ambiente. 
+        #! Aqui o agente busca explorar o ambiente.
 
         action = convert_action_number(random.randint(0, 2))
 
-    else: 
+    else:
 
-        #Aqui o agente busca pela estratégia que ele já conhece. 
+        #! Aqui o agente busca pela estratégia que ele já conhece.
 
         action = convert_action_number(better_action(curr_state))
 
-    
     print(action)
 
     state, reward = cn.get_state_reward(s, action)
 
+    print(state)
     print(reward)
-    
-    # Encontrando linha da q_table referente ao NewState
-    newState = (int(state[2:7],2) * 4) + (int(state[7:9], 2) % 4)
-    
+
+    #! Encontrando linha da q_table referente ao NewState
+    newState = (int(state, 2))
+
     update(curr_state, action, newState, reward)
     curr_state = newState
 
-    np.savetxt('resultado.txt', q_table, fmt="%f")  
+    np.savetxt("resultado.txt", q_table, fmt="%f")
